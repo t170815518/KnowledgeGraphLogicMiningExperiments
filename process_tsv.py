d10558083d766a226b88939e249bdbaa97959a9d
t170815518 <christang_1023@outlook.com>
2020-09-15 10:23:25 +0800

    Add save_target_tuples_info() method

--- process_tsv.py
+++ process_tsv.py
@@ -13,26 +13,35 @@ class TuplesHandler:
         self.sample_size_for_extremes = sample_size_for_extremes
 
         self.sample = None
+        self.target_samples = None
 
     def select_target_tuples(self):
         tsv_file = pd.read_csv(self.tsv_file_path, sep='\t')
         tsv_file.sort_values(self.target_attribute, ascending=False, inplace=True)
         original_data_size = tsv_file.shape[0]
-        random_sample_size = round(original_data_size * self.sample_size - 2 * self.sample_size_for_extremes)
+        random_sample_size = round(original_data_size * self.sample_size - 3 * self.sample_size_for_extremes)
 
         largest_tuples = tsv_file.iloc[:self.sample_size_for_extremes]
         smallest_tuples = tsv_file.iloc[-self.sample_size_for_extremes:]
+
         tsv_file = tsv_file.iloc[self.sample_size_for_extremes:-self.sample_size_for_extremes]
 
-        middle_valued_tuples = tsv_file.sample(n=random_sample_size, axis=0)
+        middle_tuples = tsv_file.sample(n=self.sample_size_for_extremes, axis=0)
+        tsv_file = tsv_file.drop(middle_tuples.index)
+        random_selected_tuples = tsv_file.sample(n=random_sample_size, axis=0)
 
-        self.sample = pd.concat([largest_tuples, smallest_tuples, middle_valued_tuples], axis=0)[['Rule']]
+        self.sample = pd.concat([largest_tuples, smallest_tuples, random_selected_tuples], axis=0)[['Rule']]
+        self.target_samples = pd.concat([largest_tuples, smallest_tuples, random_selected_tuples, middle_tuples], axis=0)[['Rule']]
         return self.sample
 
     def to_csv(self):
-        export_path = os.path.join(path, self.dataset_name, "{}_{:02f}.tsv".
+        export_path = os.path.join(path, self.dataset_name, "{}_{:2f}.tsv".
                                    format(self.dataset_name, self.sample_size))
         self.sample.to_csv(export_path, sep='\t', header=False, index=False)
+
+    def save_target_tuples_info(self):
+        export_path = os.path.join(path, self.dataset_name, "target_samples.tsv")
+        self.target_samples[['Rule']].to_csv(export_path, sep='\t')
 
 
 if __name__ == "__main__":
@@ -41,3 +50,5 @@ if __name__ == "__main__":
             handler = TuplesHandler(dataset, "Head Coverage", sample_size=sample_size)
             handler.select_target_tuples()
             handler.to_csv()
+        handler.save_target_tuples_info()
+
