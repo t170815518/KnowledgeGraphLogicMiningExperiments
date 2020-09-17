d8a37cfa1fa628480699d4e15e5e70246cfd83a5
t170815518 <christang_1023@outlook.com>
2020-09-17 19:10:44 +0800

    no message

--- process_tsv.py
+++ process_tsv.py
@@ -1,54 +1,109 @@
 import pandas as pd
 import os
+from math import ceil
 
 path = ""
 
 
 class TuplesHandler:
-    def __init__(self, dataset_name, target_attribute, sample_size, sample_size_for_extremes=5):
+    '''
+        Object takes samples based on statistics from AMIE output in tsv format, and generates new samples in different
+        size.
+    '''
+    def __init__(self, dataset_name, target_attribute, sample_size_for_extremes=5):
+        '''
+        :param dataset_name: string, used to identify the directory of the file 
+        :param target_attribute: string, "Head Convergence"
+        :param sample_size_for_extremes: int, the number of selected maximum, minimum and middle rules (e.g. 3x)
+        '''
         self.dataset_name = dataset_name
-        self.tsv_file_path = os.path.join(path, dataset_name, "AMIE_plus_result.tsv")
         self.target_attribute = target_attribute
-        self.sample_size = sample_size
         self.sample_size_for_extremes = sample_size_for_extremes
+        
+        self.AMIE_file = pd.read_csv(os.path.join(path, dataset_name, "AMIE_plus_result.tsv"), sep='\t')
+        self.tuples = pd.read_csv(os.path.join(path, dataset_name, "{}_train.tsv").format(dataset_name), sep='\t',
+                                       names=['subject', 'relation', 'object'])
+        self.tuples_num = self.tuples.shape[0]
+        self.tuples_groups = self.tuples.groupby('relation')
 
-        self.sample = None
-        self.target_samples = None
+        self.sample_size_interval = [0.1*x for x in range(1, 11)]
+        self.selected_mined_rules = None
 
-    def select_target_tuples(self):
-        tsv_file = pd.read_csv(self.tsv_file_path, sep='\t')
-        tsv_file.sort_values(self.target_attribute, ascending=False, inplace=True)
-        original_data_size = tsv_file.shape[0]
-        random_sample_size = round(original_data_size * self.sample_size - 3 * self.sample_size_for_extremes)
+    def select_mined_rules(self):
+        '''
+        Select some mined rules from AMIE with maximum, minimum and middle-range target value.
+        A tsv file will be generated in the dataset file.
+        :return pd.DataFrame, the selected mined rules
+        '''
+        self.AMIE_file.sort_values(self.target_attribute, ascending=False, inplace=True)
 
-        largest_tuples = tsv_file.iloc[:self.sample_size_for_extremes]
-        smallest_tuples = tsv_file.iloc[-self.sample_size_for_extremes:]
+        largest_tuples = self.AMIE_file.iloc[:self.sample_size_for_extremes]
+        smallest_tuples = self.AMIE_file.iloc[-self.sample_size_for_extremes:]
 
-        tsv_file = tsv_file.iloc[self.sample_size_for_extremes:-self.sample_size_for_extremes]
+        remaining_tuples = self.AMIE_file.iloc[self.sample_size_for_extremes:-self.sample_size_for_extremes]
+        middle_tuples = remaining_tuples.sample(n=self.sample_size_for_extremes, axis=0)
 
-        middle_tuples = tsv_file.sample(n=self.sample_size_for_extremes, axis=0)
-        tsv_file = tsv_file.drop(middle_tuples.index)
-        random_selected_tuples = tsv_file.sample(n=random_sample_size, axis=0)
+        self.selected_mined_rules = pd.concat([largest_tuples, smallest_tuples, middle_tuples], axis=0)
+        self.AMIE_file.drop(self.selected_mined_rules.index)
+        self.selected_mined_rules.to_csv(os.path.join(path, self.dataset_name, "selected_mined_rules.tsv"), sep='\t')
 
-        self.sample = pd.concat([largest_tuples, smallest_tuples, random_selected_tuples], axis=0)[['Rule']]
-        self.target_samples = pd.concat([largest_tuples, smallest_tuples, random_selected_tuples, middle_tuples], axis=0)[['Rule']]
-        return self.sample
+        return self.selected_mined_rules
 
-    def to_csv(self):
-        export_path = os.path.join(path, self.dataset_name, "{}_{:2f}.tsv".
-                                   format(self.dataset_name, self.sample_size))
-        self.sample.to_csv(export_path, sep='\t', header=False, index=False)
+    def parse_target_rules(df):
+        '''Create a new column in df that stores the relations appearing in the rule'''
+        def parser(rule_string):
+            relations = []
 
-    def save_target_tuples_info(self):
-        export_path = os.path.join(path, self.dataset_name, "target_samples.tsv")
-        self.target_samples[['Rule']].to_csv(export_path, sep='\t')
+            buckets, inferenced = rule_string.split("=>")
+            relations.append(inferenced.split()[1])
+            buckets = buckets.split()
+            if len(buckets) == 3:
+                relations.append(buckets[1])
+            elif len(buckets) == 6:
+                relations.extend([buckets[1], buckets[4]])
+            else:
+                print("%s cannot be parsed.".format(rule_string))
+
+            return relations
+
+        df['Relations'] = df['Rule'].apply(parser)
+
+    def generate_sample_tuples(self, frac, export=True):
+        samples = []
+        values = self.selected_mined_rules['Relations'].tolist()
+        target_relations = list(set([y for x in values for y in x]))  # get all unique relations to a list
+
+        # sample target relations in sampled rules
+        for rel in target_relations:
+            sample = self.tuples_groups.get_group(rel).sample(frac=frac, axis=0)
+            samples.append(sample)
+        print("==========Sample remaining tuples==========")
+
+        # remove the target relations from the sample
+        remaining_tuples = self.tuples.drop(self.tuples[self.tuples["relation"].apply(lambda x: x in target_relations)].index)
+        print("Total Sample Size =", self.tuples_num)
+        print("Remaining Sample Size =", remaining_tuples.shape)
+
+        samples.append(remaining_tuples.sample(frac=frac, axis=0))
+        samples = pd.concat(samples, axis=0)
+        print("{}({}) samples are selected in total.".format(samples.shape[0], samples.shape[0]/self.tuples_num))
+        print("=========Sample Completes==========")
+
+        if export:
+            samples.to_csv(os.path.join(path, self.dataset_name, "{}_{:.2f}.tsv".format(self.dataset_name, frac)),
+                           sep='\t', header=False, index=False)
+
+    def sample(self):
+        self.select_mined_rules()
+        TuplesHandler.parse_target_rules(self.selected_mined_rules)
+        for sample_size in self.sample_size_interval:
+            self.generate_sample_tuples(sample_size)
 
 
 if __name__ == "__main__":
-    for dataset in ['FB15k-237', 'wikidata_300k']:
-        for sample_size in [0.1*x for x in range(1, 11)]:
-            handler = TuplesHandler(dataset, "Head Coverage", sample_size=sample_size)
-            handler.select_target_tuples()
-            handler.to_csv()
-        handler.save_target_tuples_info()
+    for dataset in ['wikidata_300k',]:
+        print("==========Sampling {}==========".format(dataset))
+        handler = TuplesHandler(dataset, "Head Coverage")
+        handler.sample()
+        print("==========Finish sampling {}==========".format(dataset))
 
---
+++ result_parser.py
@@ -0,0 +1,51 @@
+from process_tsv import TuplesHandler
+import pandas as pd
+import os
+from numpy import nan
+
+path = ""
+
+
+class ResultParser:
+    '''parse the result from amie output'''
+    def __init__(self, dataset, target_attribute):
+        self.dataset = dataset
+        self.target_attribute = target_attribute
+        self.selected_rules = pd.read_csv(os.path.join(path, dataset, "selected_mined_rules.tsv"), sep='\t', index_col=0)
+        self.target_attribute_indexes = {"Head Coverage": -10, "Std Confidence": -9, "PCA Confidence": -8}
+
+        TuplesHandler.parse_target_rules(self.selected_rules)
+        self.selected_rules[target_attribute] = 0
+
+    def parse_amie_output(self, file_path):
+        ''' find the target relations and return their score '''
+        def is_contain_relations(l):
+            for rel in l:
+                if rel not in line:
+                    return False
+            return True
+
+        with open(file_path, 'r') as f:
+            for line in f:
+                if line[0] == '?':  # data line starts with ? e.g. ?a
+                    split_lines = line.split()
+                    for index, row in self.selected_rules.iterrows():
+                        if is_contain_relations(row['Relations']):
+                            value = float(split_lines[self.target_attribute_indexes[self.target_attribute]])
+                            self.selected_rules.loc[index, self.target_attribute] = value
+                            break
+
+        return self.selected_rules
+
+
+if __name__ == '__main__':
+    dataset = "wikidata_300k"
+    target_attribute = "Head Coverage"
+
+    parser = ResultParser(dataset, target_attribute)
+    result = parser.selected_rules
+    for size in ["0.10", '0.20', "0.30", "0.40", "0.50", "0.60", "0.70", "0.80", "0.90", "1.00"]:
+        buffer = parser.parse_amie_output(os.path.join(path, dataset, "{}_{}.txt".format(dataset, size)))
+        result[size] = buffer[target_attribute]
+        print("{} has been parsed.".format(size))
+    result.to_csv(os.path.join(path, dataset, "result.csv"))
